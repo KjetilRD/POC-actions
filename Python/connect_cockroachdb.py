1e230271d5800bfe58986bc016f46c589d68d9a3
KjetilRD <38722744+KjetilRD@users.noreply.github.com>
2022-06-20 07:19:38 +0200

    Update connect_cockroachdb.py

--- Python/connect_cockroachdb.py
+++ Python/connect_cockroachdb.py
@@ -1,6 +1,6 @@
 
 
-#This should connnect to Cockroachdb.
+# This should connnect to Cockroachdb.
 
 
 from sqlalchemy import create_engine
@@ -8,4 +8,3 @@ import os
 
 engine = create_engine(os.environ['DATABASE_URL'])
 engine.connect()
-
