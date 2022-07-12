3a6515aa2f7d22cbac1db791867f56ba61ccb9c6
KjetilRD <38722744+KjetilRD@users.noreply.github.com>
2022-07-12 21:49:24 +0200

    Update get_rest.py

--- app/get_rest.py
+++ app/get_rest.py
@@ -6,7 +6,7 @@ def main():
 
     response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
     print(response.status_code)
-    json_raw = response.json())
+    json_raw = response.json()
     json_formed = json.dumps(raw, sort_keys=True, indent=4)
     print(json_formed)
 
