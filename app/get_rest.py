e884ebb62f99d853046d5e630b38fcf011c5eb5d
KjetilRD <38722744+KjetilRD@users.noreply.github.com>
2022-08-10 10:18:06 +0200

    Update get_rest.py

--- app/get_rest.py
+++ app/get_rest.py
@@ -9,13 +9,12 @@ def main():
     json_raw = response.json()
     json_formed = json.dumps(json_raw, sort_keys=True, indent=4)
     print(json_formed)
-    #print(json_raw['bpi'])
     bpi = json_raw['bpi']
-    print(bpi['USD'])
     for ccy in bpi:
         print('Iterating:')
         print(ccy)
         print(bpi[ccy])
-    
+
+
 if __name__ == "__main__":
     main()
